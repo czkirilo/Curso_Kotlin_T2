242ebd2a05c858f2115ea68980eef53e60e3d334
Majorbig <caiquez.kirilo@hotmail.com>
2021-09-03 21:00:43 -0300

    finalização primeira parte do Dice Roller

--- Dado/app/src/main/java/com/example/dado/MainActivity.kt
+++ Dado/app/src/main/java/com/example/dado/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.dado
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            val resultTextView: TextView = findViewById(R.id.textView)
+            rollButton.setOnClickListener {
+                rollDice()
+            }
+
+        }
+
     }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+
+    }
+
+    class Dice(private val numSides: Int) {
+
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
+    }
+
 }
